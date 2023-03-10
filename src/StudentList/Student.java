4104e55d7c5bb4ff3e50029b0aad6477ac862f49
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-10 20:38:37 +0100

    add parseStudent()

--- src/StudentList/Student.java
+++ src/StudentList/Student.java
@@ -11,6 +11,9 @@ package StudentList;
 
 public class Student {
 
+  final private static char SEP = ',';
+
+  private int id;
   private String name;
   String group;
   String eMail = null;
@@ -48,5 +51,16 @@ public class Student {
 
   public String geteMail() {
     return eMail;
+  }
+
+  public static Student parseStudent(String group, String line) {
+    int sepIndex = line.indexOf(SEP);
+    if (sepIndex != -1) {
+      String name = line.substring(0, sepIndex);
+      String eMail = line.substring(sepIndex +1);
+      return new Student(name, group, eMail);
+    }
+      return new Student(/*name*/line, group);
+    }
   }
 }
